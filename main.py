59a6fd22be590fc984dd37eca7f9de49b4a969e5
Klihe <stepan.birke@gmail.com>
2023-11-26 19:26:32 +0100

    Movement & Player2

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ class Player():
         self.speed = speed
         self.color = color
 
+    def movement(self, up, down):
+        if keys[up] and self.y > 0:
+            self.y -= self.speed
+        if keys[down] and self.y < winHeight - self.height:
+            self.y += self.speed
+
     def update(self, win):
         pygame.draw.rect(win, WHITE, (self.x, self.y, self.width, self.height))
 
@@ -35,10 +41,12 @@ class Player():
 def winUpdate():
     winMain.fill(BLACK)
     player1.update(winMain)
+    player2.update(winMain)
     pygame.display.update()
 
 # Create objects
 player1 = Player(BLOCK, winHeight / 2, BLOCK / 2, BLOCK * 2, 10, WHITE)
+player2 = Player(winWidth - BLOCK - BLOCK / 2, winHeight / 2, BLOCK / 2, BLOCK * 2, 10, WHITE)
 
 # Game
 run = True
@@ -53,6 +61,9 @@ while run:
 
     if keys[pygame.K_ESCAPE]:
             game_display = pygame.display.set_mode((winWidth, winHeight))
+
+    player1.movement(pygame.K_w, pygame.K_s)
+    player2.movement(pygame.K_UP, pygame.K_DOWN)
 
     winUpdate()
 
