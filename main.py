ab06deb174dfcbc172e570706f377793a99f681a
Klihe <stepan.birke@gmail.com>
2023-11-26 18:56:48 +0100

    Config, Settings & MainLoop

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import pygame
 
 pygame.init()
+
+# Settings
+winWidth = 1920
+winHeight = 1080
+
+# Config
+winMain = pygame.display.set_mode((winWidth, winHeight))
+clock = pygame.time.Clock()
+
+# Game
+run = True
+while run:
+    clock.tick(60)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_ESCAPE]:
+            game_display = pygame.display.set_mode((winWidth, winHeight))
+
+pygame.quit()
