23a41470bb5cd217c4abd97bcd6ba62a8920e073
Klihe <stepan.birke@gmail.com>
2023-12-06 19:34:05 +0100

    Pause

--- main.py
+++ main.py
@@ -17,24 +17,28 @@ game = Game()
 # Main game loop
 clock = pygame.time.Clock()
 running = True
+pause = False
 
 while running:
     clock.tick(60)  # Set the frame rate
-    
-    # Handle events
-    game.handleEvents()
-    # Update game state
-    game.update()
-    # Draw the game
-    game.draw(winMain)
-
     # Check for quit event
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pause = not pause
+    
+    if not pause:
+        # Handle events
+        game.handleEvents()
+        # Update game state
+        game.update()
+        # Draw the game
+        game.draw(winMain)
 
-    # Update the display
-    pygame.display.flip()
+        # Update the display
+        pygame.display.flip()
 
 # Quit Pygame
 pygame.quit()
