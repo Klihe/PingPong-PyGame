89b74a6cc35054695362e39a42e2613001a3a7d1
Klihe <stepan.birke@gmail.com>
2023-12-06 17:51:11 +0100

    Clear menu

--- modules/gameState/menu.py
+++ modules/gameState/menu.py
@@ -9,7 +9,6 @@ def drawMenu(win, windowWidth, windowHeight, players) -> None:
     nameFont = pygame.font.Font(None, 120)
     statsFont = pygame.font.Font(None, 48)
     charFont = pygame.font.Font(None, 200)
-    text = menuFont.render("Press SPACE to Start", True, Color.WHITE)
     # Player 1
     # background
     pygame.draw.rect(win, Color.WHITE, (60, 60, 870, 960))
@@ -57,5 +56,3 @@ def drawMenu(win, windowWidth, windowHeight, players) -> None:
     # specialAbility = statsFont.render("Special Ability: " + str(players[0].score), True, Color.WHITE)
     # ability
     pygame.draw.rect(win, Color.BLACK, (510 + 930, 630, 810 / 2 - 15, 750 / 2 - 15))
-
-    win.blit(text, (windowWidth // 2 - text.get_width() // 2, windowHeight // 2 - text.get_height() // 2))
